506956d39d958d1a6561eaf7362060a3992f24d5
Dushan Randika <dushanrandika1994@gmail.com>
2019-05-09 22:43:35 +0530

    Added main widget topics for the content

--- src/App.tsx
+++ src/App.tsx
@@ -25,26 +25,40 @@ class App extends Component<any, any> {
                 <TestCompo name="Randika" /> */}
                 <Navbar image={img}/>
                 <Container text style={{ marginTop: '7em' }}>
-                    <Header as='h1' >Ecommerce widgets</Header>
+                    <Header as='h1' >Widgets</Header>
                     <List divided relaxed>
                         <List.Item>
                             <List.Icon name='github' size='large' verticalAlign='middle' />
                             <List.Content>
-                                <List.Header as='a'>Semantic-Org/Semantic-UI</List.Header>
+                                <List.Header as='a'>Search Bar</List.Header>
                                 <List.Description as='a'>Updated 10 mins ago</List.Description>
                             </List.Content>
                         </List.Item>
                         <List.Item>
                             <List.Icon name='github' size='large' verticalAlign='middle' />
                             <List.Content>
-                                <List.Header as='a'>Semantic-Org/Semantic-UI-Docs</List.Header>
+                                <List.Header as='a'>Social Media Widget</List.Header>
                                 <List.Description as='a'>Updated 22 mins ago</List.Description>
                             </List.Content>
                         </List.Item>
                         <List.Item>
                             <List.Icon name='github' size='large' verticalAlign='middle' />
                             <List.Content>
-                                <List.Header as='a'>Semantic-Org/Semantic-UI-Meteor</List.Header>
+                                <List.Header as='a'>Community, Comments and Forum</List.Header>
+                                <List.Description as='a'>Updated 34 mins ago</List.Description>
+                            </List.Content>
+                        </List.Item>
+                        <List.Item>
+                            <List.Icon name='github' size='large' verticalAlign='middle' />
+                            <List.Content>
+                                <List.Header as='a'>Booking Widget</List.Header>
+                                <List.Description as='a'>Updated 34 mins ago</List.Description>
+                            </List.Content>
+                        </List.Item>
+                        <List.Item>
+                            <List.Icon name='github' size='large' verticalAlign='middle' />
+                            <List.Content>
+                                <List.Header as='a'>Customer Service Widget</List.Header>
                                 <List.Description as='a'>Updated 34 mins ago</List.Description>
                             </List.Content>
                         </List.Item>
